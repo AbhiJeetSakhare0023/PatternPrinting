88f3eb6517382567f6b4982fad6ba2c0cdb93161
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-03-03 23:29:56 +0530

    Star printing in rectangle format i.e 3 row 5 column

---
+++ src/Roww3Coloumn5.java
@@ -0,0 +1,15 @@
+public class Roww3Coloumn5 {
+
+        public static void main(String[] args) {
+            for(int i=1;i<=3;i++) // outer loop for row purpose
+            {
+                for(int j=1;j<=5;j++) // inner loop for column purpose
+                {
+                    System.out.print(" * ");
+                }
+                System.out.println(" ");
+            }
+        }
+
+
+}
