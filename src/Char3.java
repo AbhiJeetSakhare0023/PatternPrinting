6c39b1e12f87d16ebf5bcd82a5d19c3308ab1e7f
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-03-04 01:29:54 +0530

    Character Pattern 3

---
+++ src/Char3.java
@@ -0,0 +1,22 @@
+public class Char3 {
+    public static void main(String[] args) {
+        char c='A';
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=1;j<=3;j++)
+            {
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.println(" ");
+        }
+    }
+}
+/*
+o/p:
+
+A B C
+D E F
+G H I
+
+ */
