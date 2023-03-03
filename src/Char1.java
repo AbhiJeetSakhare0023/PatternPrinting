36ecb788192b0263a500fd2c0e20c6e505a675c0
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-03-04 01:18:16 +0530

    Character Pattern 1

---
+++ src/Char1.java
@@ -0,0 +1,23 @@
+public class Char1 {
+    public static void main(String[] args) {
+        char c='A';
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=1;j<=3;j++)
+            {
+                System.out.print(c+" ");
+            }
+            c++;
+            System.out.println(" ");
+        }
+    }
+
+}
+/*
+o/p:
+
+A A A
+B B B
+C C C
+
+ */
